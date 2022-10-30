fa7d33d6b7062fcf3bbdeed8d00c3488bb756ea8
Daniel Thorp <arcanistzed@gmail.com>
2022-10-30 03:00:06 +0000

    feat: added welcome to api message

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,16 @@
+import type { Request, Response } from "express";
 import express, { Router } from "express";
 import auth, { isAuthenticated } from "./api/auth.js";
 import users from "./api/users.js";
 
 const router = Router();
+
+router.get("/", (_req: Request, res: Response) => {
+    res.json({
+        success: true,
+        message: "Welcome to the API",
+    });
+});
 
 // Check if the user is authenticated
 router.use(isAuthenticated);
